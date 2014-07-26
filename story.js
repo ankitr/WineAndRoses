5863a0f9942e2183bad3765fce5b9a2d7467bd5e
Ankit Ranjan <me@ankit.io>
2014-07-26 12:22:42 -0700

    added the end

--- story.js
+++ story.js
@@ -4,7 +4,7 @@ var life = {
     start: {
         description: 'You\'re sitting at a bus stop. A girl comes up and sits next to you.',
         choices: {
-            'Say hi.': {
+            'Say hi': {
                 callback: function (world, log) {
                     world.you.curiosity = 1;
                     world.you.distraction = 1;
@@ -13,13 +13,27 @@ var life = {
                 },
                 next: 'hey'
             },
-            'Avoid eye contact.':{
+            'Avoid eye contact':{
                 callback: function (world, log) {
                     world.me.mystery = 1;
                     world.me.courage = 0;
                     world.us.significance = 0;
                 },
                 next: 'bus'
+            }
+        }
+    },
+    end: {
+        description: 'It\'s time for her to go. You look onwards. Slowly, she dissapears into the horizon.',
+        choices: {
+            'Move on': {
+                callback: function (world, log) {
+                    world.you = {};
+                    world.me = {};
+                    world.us = {};
+                    log('Years move on. Life moves on. And you learn to start over.');
+                },
+                next: 'start'
             }
         }
     }
