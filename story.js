8a285d41355aca7b2573362fa370c8de72de8663
Ankit Ranjan <me@ankit.io>
2014-07-26 01:08:36 -0700

    Removed useless code.

--- story.js
+++ story.js
@@ -4,20 +4,14 @@ var you;
 var me;
 var us;
 
-function Decision (description, choices, color) {
-    this.description = description;
-    this.choices = choices;
-    this.color = color;
-}
-
 var life = {
-    'start': new Decision(
+    start: {
         // Figure gender out.
-        'You\'re sitting at a bus stop. A person comes up and sits next to you.',
-        {
+        description: 'You\'re sitting at a bus stop. A person comes up and sits next to you.',
+        choices: {
              'Say hi.':{},
              'Avoid eye contact.':{}
         },
-        '#fff'
-    )
+        color: '#fff'
+    }
 }
