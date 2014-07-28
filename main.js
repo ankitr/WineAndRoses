ac8583f4c7bc948124c3c1602b096f8215891398
Hardmath123 <hardmath123@gmail.com>
2014-07-27 23:20:14 -0700

    baud

--- main.js
+++ main.js
@@ -4,17 +4,45 @@
     var currentactive = null;
     function log(text) {
         if (arguments.length === 1) {
-            var el = document.createElement("div");
-            var p = document.createElement("p");
-            p.innerText = text;
-            el.appendChild(p);
-            document.getElementById("gamewrapper").appendChild(el);
+            baudqueue.push(text);
         } else {
             [].forEach.call(arguments, function(line) {
                 log(line[0] + ': ' + line[1]);
             });
         }
     }
+
+    var baudqueue = [];
+    var baudindex = 0;
+    function baud() {
+        if (baudindex < baudqueue.length) {
+            var text = baudqueue[baudindex];
+            if (typeof(text) === 'string') {
+                var p = document.createElement("p");
+                document.getElementById("gamewrapper").appendChild(p);
+
+                var charindex = 0;
+                function char() {
+                    if (charindex < text.length) {
+                        p.textContent += text[charindex];
+                        charindex++;
+                        window.setTimeout(char, 10);
+                    } else {
+                        window.setTimeout(baud, 10);
+                    }
+                }
+                char();
+            } else {
+                document.getElementById("gamewrapper").appendChild(text);
+                window.setTimeout(baud, 10);
+            }
+            baudindex++;
+        } else {
+            window.setTimeout(baud, 10);
+        }
+    }
+    baud();
+
     function rungame(node, key) {
         var active = true;
         if (node[key].color) {
@@ -54,7 +82,8 @@
             l.appendChild(li);
         })
         el.appendChild(l);
-        document.getElementById("gamewrapper").appendChild(el);
+        // document.getElementById("gamewrapper").appendChild(el);
+        log(el);
     }
 
     window.addEventListener('load', function() {
